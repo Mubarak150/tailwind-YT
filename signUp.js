437606c43db7e60440a79d60e86314fc22a94d71
Mubarak150 <mkmubarak2347@gmail.com>
2023-09-19 06:16:31 -0700

    nnn

--- signUp.js
+++ signUp.js
@@ -1,6 +1,14 @@
-alert("connet");
+var value1;
+var alertText;
 
-var text = $(".firstName");
-$(".sign-up").click(function() {
-    alert(text);
-})
+$("#first-name").on("change", function () {
+    alertText = $("#first-name-alert");
+    alertText.removeClass("hidden");
+    value1 = $(this).val();
+    console.log(value1);
+    if (value1.length < 1) {
+        alertText.text("This field is mandatory...");
+    } else {
+        alertText.text("go ahead!");
+    }
+});
