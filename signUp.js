39228576f11db0e44d819312454c435ee01576fb
Mubarak150 <mkmubarak2347@gmail.com>
2023-09-20 08:01:22 -0700

    password justification

--- signUp.js
+++ signUp.js
@@ -1,18 +1,79 @@
-var value1;
-var alertText;
+$(document).ready(function() {
+   var stop = false;
+   var appeared = false;
+   var appeared2=true;
+   var confirmPassword = 0 ;
+   var password = 0;
+   var message2; 
+   
 
-$("#first-name").on("change", function () {
-    alertText = $("#first-name-alert");
-    alertText.removeClass("hidden");
-    value1 = $(this).val();
-    console.log(value1);
-    if (value1.length < 1) {
-        alertText.text("This field is mandatory...");
-    } else {
-        alertText.text("go ahead!");
-    }
-});
+    $(".name").on("input", function() {
+      var one = $(this).val(); 
+      console.log(one);
+      console.log(one.length)
+        
+        if(one === "" && !appeared ) { 
+        appeared = true;
+        var message = $(this).after("<p class='fill-alert text-yellow-200 font-bold text-sm'>* please fill this portion...</p>");
+        $("#sign-up").prop("disabled", true);
+        $("#sign-up").css("backgroundColor", "red")
+        $("#sign-up").css("color", "white")
+        $(this).css("border", "2px solid red")
+        
+        }
+        else {
+          appeared = false;
+          message = $(this).next(".fill-alert").remove();
+          $("#sign-up").prop("disabled", false);
+          $("#sign-up").css("backgroundColor", "")
+          $("#sign-up").css("color", "")
+          $(this).css("border", "")
+        }
+      
+    }) // up to this line the code is fine for me.
 
-$("#h1").click(function() {
-    $("#h1").css("color", "yellow");
+    ////////// pass word justification 
+    $("#password").on("input", function() {
+      password = $("#password").val(); 
+
+    })
+    
+    $("#confirm-password").on("input", function() {
+      confirmPassword = $("#confirm-password").val(); 
+      // console.log(confirmPassword);
+      // console.log(confirmPassword.length)
+      // console.log(password);
+      // console.log(password.length)
+      appeared2=true;
+
+      // if(password.length === confirmPassword.length ) {
+        if(password !== confirmPassword && appeared2)  {
+            appeared2 = true;
+            message2 = $(".match-alert").remove();
+            message2 = $("#confirm-password").before("<p class='match-alert text-yellow-200 font-bold text-sm'>** passwords must match...</p>");
+            $("#sign-up").prop("disabled", true);
+            $("#sign-up").css("backgroundColor", "red")
+            $("#sign-up").css("color", "white")
+            $("#password").css("border", "2px solid red")
+            $("#confirm-password").css("border", "2px solid red")
+            console.log("ture")
+
+            }
+      else {
+        message2 = $(".match-alert").remove();
+        $("#sign-up").prop("disabled", false);
+        $("#sign-up").css("backgroundColor", "")
+        $("#sign-up").css("color", "")
+        $("#password").css("border", "")
+        $("#confirm-password").css("border", "")
+        appeared2 = false;
+        console.log("falsss")
+            
+          }
+    })
+ 
 })
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+ 
